c47eab8f41dcc131bec47b4e2ed4064291b57981
amritpdl <pdlamrit123@gmail.com>
2017-11-03 16:28:35 +0545

    Checking at phage and correcting them if they are error

--- Corrector.php
+++ Corrector.php
@@ -7,9 +7,16 @@
 		$size = count($arrFilename);
 
 		$fileLastName = $arrFilename[$size - 1];
+		if(is_numeric($filename[0])){
+			echo "The file name can not start from number";
+		}elseif (strlen($filename)==1) {
+			echo "The file name should be more then a single letter";
+		}
+		else{
 
-		$correctFileName = $customerName. '-' .$projectName. '-' .$fileLastName;
+			$correctFileName = $customerName. '-' .$projectName. '-' .$fileLastName;
 
-		echo "Correct File Name: ".$correctFileName;
+			echo "Correct File Name: ".$correctFileName;
+		}	
 	}
 ?>	
