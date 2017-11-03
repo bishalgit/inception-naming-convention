616ec34593b75619ad10df78b6d18e4e9f974fc8
Susan <susan.kc13@gmail.com>
2017-11-03 11:04:10 +0545

    fileManager.php is added in order to get input from the index.php

---
+++ fileManager.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	//$_POST take the input from the post
+	/*var customerName= $_POST["customer"];
+	var projectName= $_POST["project"];
+	var fileName= $_POST["file"];*/
+
+	//Testing the input file
+	$customerName= "susan";
+	$projectName= "inception";
+	$fileName= "susan.doc";
+
+	$convec = $customerName . "-" . $projectName . "-" .$fileName;
+	echo $convec;
+
+
+
+
+
+
+ ?>
